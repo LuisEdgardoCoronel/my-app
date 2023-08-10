c1629d3be34165ac38d0b07024b00ff9d4a96018
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-10 20:37:01 -0300

    aprendiendo tipos globales

---
+++ my-app/app.d.ts
@@ -0,0 +1,9 @@
+type IFoxItems = {id:string, url:string}//tipos globales
+/**
+ * Creando tipos propios para la aplicación
+Es bueno pero también malo tener tipos globables. Analiza 
+bien tu necesidad de negocio para saber qué sí debería ser 
+global. No abuses de los tipos globales.
+
+siempre es mejor exportar
+ */
